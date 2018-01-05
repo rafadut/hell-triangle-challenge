7273381e92a4f90436b7af56e305c68c17a3cb4b
Rafael Dutra <rafaeldut89@gmail.com>
2018-01-05 15:19:41 -0200

    Adicionar arquivos de projeto.

---
+++ hell-triangle-challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hell_triangle_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
